f3311fe7c98225c56a2b142ce1444c2e57511361
PUGAZHENDHI V <126157273+PugazhendhiVelu@users.noreply.github.com>
2024-10-08 14:47:06 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -19,25 +19,27 @@ const redirectUrl = process.env.REDIRECT_URL;
 console.log(redirectUrl);
 
 app.use(express.json());
-const allowedOrigin = ['https://nex-gen-learn-deployment-frontend.vercel.app'];
+const allowedOrigin = 'https://nex-gen-learn-deployment-frontend.vercel.app';
 app.use(cors({
-    origin: allowedOrigin, // Replace with your frontend URL
-    credentials: true // Allow cookies to be sent and received
+    origin: allowedOrigin, // Allow specific frontend URL
+    credentials: true, // Allow cookies and headers
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed methods
+    allowedHeaders: ['Content-Type', 'Authorization'] // Allowed headers
 }));
 
- const access = (req,res,next)=>{
+const access = (req, res, next) => {
     const origin = req.headers.origin;
     if (origin !== allowedOrigin) {
         return res.status(403).sendFile(path.join(__dirname, 'template', 'Error403.html'));
-        // return res.status(403).send('Access denied. Invalid origin , You are not authorised to access this resource.');
     }
     next();
-} 
+};
+
 app.use(access);
 app.use('/api/courses', CourseRoute);
-app.use('/api/instructor',InstructorRoute);
-app.use('/api/user',UserRoute);
-app.use('/api/quiz',QuizRoute);
+app.use('/api/instructor', InstructorRoute);
+app.use('/api/user', UserRoute);
+app.use('/api/quiz', QuizRoute);
 
 // Start the server
 app.listen(process.env.PORT, () => {
